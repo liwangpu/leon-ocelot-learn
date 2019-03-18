f11e14ed036da973077f168b688504ffea9dc207
leon <leon.pu@gmail.com>
2019-03-18 15:36:30 +0800

    实施空的ocelot apigateway

---
+++ App-APIGateway/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace App_APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                    config.AddCommandLine(args);
+                })
+                .UseStartup<Startup>();
+            return builder;
+        }
+
+    }
+}
